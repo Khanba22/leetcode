27ffad80642fb392b1d9c06b29047b0582066716
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-17 10:31:15 +0530

    Time: 0 ms (100%), Space: 52.2 MB (75.47%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,12 +3,11 @@ public:
     
     bool canJump(vector<int>& nums) {
         int n = nums.size() - 1;
-        int last = n;
         for(int i = n;i >= 0;i--){
-            if(i+nums[i] >= last){
-                last = i;
+            if(i+nums[i] >= n){
+                n = i;
             }
         }
-        return !last;
+        return !n;
     }
 };
