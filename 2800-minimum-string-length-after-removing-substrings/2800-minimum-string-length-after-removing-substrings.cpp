e2fd13a81e9125cb92baf3a87789633394ac6183
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-17 11:38:14 +0530

    Time: 6 ms (28.03%), Space: 10.5 MB (78.5%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minLength(string s) {
         if(s.size() <= 1) return s.size();
-        string stack = {s[0]};
+        string stack = string(1,s[0]);
         for(short i = 1;i < s.size();i++){
             if(!stack.empty() && ((stack.back() == 'A' && s[i] == 'B') || (stack.back() == 'C' && s[i] == 'D'))) stack.pop_back();
             else stack.push_back(s[i]);
