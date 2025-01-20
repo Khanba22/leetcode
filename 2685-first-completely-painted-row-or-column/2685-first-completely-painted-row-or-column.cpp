4b01762d30f4755b34c6232fda6ae000a735f89c
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-20 20:46:30 +0530

    Time: 0 ms (100%), Space: 133.8 MB (91.77%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        map<int, pair<int,int>> pos;
         int m = mat.size();
         int n = mat[0].size();
         vector<int> lir(m, n);
         vector<int> lic(n, m);
+        vector<pair<int,int>>pos((m*n)+1);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 pos[mat[i][j]] = {i, j};
