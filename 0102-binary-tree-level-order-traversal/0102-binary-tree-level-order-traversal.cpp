940f48287da674903d654326ba044e9ce61ef973
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-20 19:53:01 +0530

    Time: 1 ms (40.9%), Space: 16.8 MB (87.03%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,31 +11,35 @@
  */
 class Solution {
 public:
-    void helper(TreeNode * root,map<int,vector<int>>&s,int level){
-        if(root == NULL) return;
-        if(s.find(level)!= s.end()){
-            s[level].push_back(root->val);
-        }else{
-            s[level] = {root->val};
-        }
-        helper(root->left,s,level+1);
-        helper(root->right,s,level+1);
-    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>res;
-        // deque<pair<int,int>>q;
-        // if(root == nullptr) return res;
-        // q.push_back({root->val,0});
-        // while(!q.empty()){
-        //     auto p = q.front();
-        //     q.pop_front();
-            
-        // }
-        map<int,vector<int>>s;
-        helper(root,s,0);
-        for(auto &i:s){
-            res.push_back(i.second);
+        if (!root) return {};
+
+        deque<pair<int, TreeNode*>> q;
+        q.push_back({0, root});
+
+        vector<vector<int>> res;
+
+        while (!q.empty()) {
+            auto elem = q.front();
+            q.pop_front();
+
+            int level = elem.first;
+            TreeNode* node = elem.second;
+
+            if (res.size() == level) {
+                res.push_back({node->val});
+            } else {
+                res[level].push_back(node->val);
+            }
+
+            if (node->left) {
+                q.push_back({level + 1, node->left});
+            }
+            if (node->right) {
+                q.push_back({level + 1, node->right});
+            }
         }
+
         return res;
     }
 };
