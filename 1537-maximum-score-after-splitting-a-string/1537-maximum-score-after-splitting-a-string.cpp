8b536468064ba4af7728eacba59dbabac65ee249
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-21 21:46:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (7.32%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.size();
-        vector<int> zSum(n, 0);
-        vector<int> oSum(n, 0);
+        vector<int> zSum(n, 0); 
+        vector<int> oSum(n, 0); 
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
             zSum[i] = (s[i] == '0' ? 1 : 0) + (i > 0 ? zSum[i - 1] : 0);
-        }
 
-        for (int i = n - 1; i >= 0; i--) {
-            oSum[i] = (s[i] == '1' ? 1 : 0) + (i < n - 1 ? oSum[i + 1] : 0);
+            oSum[j] = (s[j] == '1' ? 1 : 0) + (j < n - 1 ? oSum[j + 1] : 0);
         }
 
         int maxSum = INT_MIN;
