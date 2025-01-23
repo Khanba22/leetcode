eb0bb0cfff0f7466e11208da62ad8ff0c2764c99
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-23 21:54:05 +0530

    Time: 2 ms (88.54%), Space: 9.8 MB (71.15%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    string helper(string &s, int n) {
-        if (n == 1) return "1";
-        string res = "";
-        char current = s[0];
-        int currCount = 1;
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] == current) {
-                currCount++;
-            } else {
-                res += to_string(currCount) + current;
-                current = s[i];
-                currCount = 1;
-            }
-        }
-        res += to_string(currCount) + current;
-        
-        return res;
-    }
-
     string countAndSay(int n) {
-        string start = "1";
+        string res = "1";
         for (int i = 1; i < n; i++) {
-            start = helper(start, 2);
+            string next = "";
+            char current = res[0];
+            int count = 1;
+            for (int j = 1; j < res.size(); j++) {
+                if (res[j] == current) {
+                    count++;
+                } else {
+                    next += to_string(count) + current;
+                    current = res[j];
+                    count = 1;
+                }
+            }
+            next += to_string(count) + current;
+            res = next;
         }
-        return start;
+        return res;
     }
 };
