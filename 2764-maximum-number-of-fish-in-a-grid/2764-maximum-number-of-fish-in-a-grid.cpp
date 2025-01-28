4cb42a26554f84fb79c80541b9fd81076477b938
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-01-28 20:27:16 +0530

    Time: 3 ms (84.06%), Space: 94.1 MB (85.64%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int findFish(vector<vector<int>>& grid, int i, int j, int m, int n) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        int fishCount = 0;
-        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        while (!q.empty()) {
-            auto x = q.front();
-            q.pop();
-            if (x.first < 0 || x.first >= m || x.second < 0 || x.second >= n || grid[x.first][x.second] == 0) {
-                continue;
-            }
-            fishCount += grid[x.first][x.second];
-            grid[x.first][x.second] = 0;
-            for (auto dir : dirs) {
-                q.push({x.first + dir.first, x.second + dir.second});
-            }
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
         }
-        return fishCount;
+        int fish = grid[i][j];
+        grid[i][j] = 0;
+        fish += findFish(grid, i - 1, j, m, n);
+        fish += findFish(grid, i, j + 1, m, n);
+        fish += findFish(grid, i + 1, j, m, n);
+        fish += findFish(grid, i, j - 1, m, n);
+        return fish;
     }
 
     int findMaxFish(vector<vector<int>>& grid) {
@@ -26,9 +19,7 @@ public:
         int n = grid[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] > 0) {
-                    maxFish = max(maxFish, findFish(grid, i, j, m, n));
-                }
+                maxFish = max(maxFish, findFish(grid, i, j, m, n));
             }
         }
         return maxFish;
