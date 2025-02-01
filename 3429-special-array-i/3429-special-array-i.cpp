eaed6a3abda3a36d1e47b602f4fe4faeee3e16d0
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-01 22:44:55 +0530

    Time: 0 ms (100%), Space: 27.8 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         bool flag = nums[0] % 2 == 0;
-        for(int i = 1;i <nums.size();i++){
+        for(int i = 1;i < nums.size();i++){
             if(flag == (nums[i]%2 == 0) ) return false;
             flag = !flag;
         }
