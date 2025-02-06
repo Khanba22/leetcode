19b1d19d251994fa836a17bced50b1ca0ff1ae18
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-06 20:58:05 +0530

    Time: 301 ms (78.37%), Space: 76 MB (88.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        int count = 0;
-        map<int, int> prods;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                int prod = nums[i] * nums[j];
-                count += 8*prods[prod];
-                prods[prod]++;
-            }
+        const int n=nums.size();
+        unordered_map<int, int> freq;// hash map less efficient
+        freq.reserve(n*(n-1)/2);// reserve space
+        for(int i=0; i<n-1; i++)
+            for(int j=i+1; j<n; j++)
+                freq[nums[i]*nums[j]]++;
+        // no need for sort
+        int ans=0;
+        for(auto& [x, f]: freq){ // easy single loop
+            if (f>1) ans+=f*(f-1)*4;
         }
-        return count;
+        return ans;
     }
 };
