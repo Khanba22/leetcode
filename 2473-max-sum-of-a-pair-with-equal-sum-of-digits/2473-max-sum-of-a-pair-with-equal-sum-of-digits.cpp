9cad5e20ce152f14c5765fd3d903deb671405fd6
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-12 18:44:17 +0530

    Time: 114 ms (7.65%), Space: 64 MB (52.02%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int getDigitSum(int num){
-        int res = 0;
-        while(num){
-            res+=num%10;
-            num/=10;
+    int getDigitSum(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
         }
-        return res;
+        return sum;
     }
+
     int maximumSum(vector<int>& nums) {
-        unordered_map<int,vector<int>>sum;
-        int res = -1;
-        for(int &i:nums){
-            int s = getDigitSum(i);
-            if(sum.count(s)){
-                if(i >= sum[s][1]){
-                    sum[s][0] = sum[s][1];
-                    sum[s][1] = i;
-                }else if(i > sum[s][0]){
-                    sum[s][0] = i;
-                }
-            }else{
-                sum[s] = {-1,i};
+        unordered_map<int, pair<int, int>> sumMap; 
+        int maxSum = -1;
+
+        for (int num : nums) {
+            int s = getDigitSum(num);
+
+            auto& [first, second] = sumMap[s]; 
+
+            if (num >= first) {
+                second = first;
+                first = num;
+            } else if (num > second) {
+                second = num;
             }
         }
-        for(auto &x:sum){
-            if(x.second[0] != -1){
-                res = max(res,x.second[0] + x.second[1]);
+
+        for (const auto& [key, value] : sumMap) {
+            if (value.second != 0) {
+                maxSum = max(maxSum, value.first + value.second);
             }
         }
-        return res;
+
+        return maxSum;
     }
 };
