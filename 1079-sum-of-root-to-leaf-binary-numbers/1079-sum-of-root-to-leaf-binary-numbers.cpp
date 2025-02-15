8fc7891ea977aad367d61ae30163941bd1d6efd8
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-16 00:45:42 +0530

    Time: 0 ms (100%), Space: 17.4 MB (35.37%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
@@ -2,28 +2,23 @@
 class Solution {
 public:
     void preOrder(TreeNode *root,int &ans,int digit){
-        //base case
         if(root == NULL){
             return ;
         }
 
-        //binary to decimal convertion 
         digit = (digit << 1) | root -> val;
 
-        //leaf node
         if(root -> left == NULL && root -> right == NULL){
             ans += digit;
             return ;
         }
 
-        //recursive call
         preOrder(root -> left,ans,digit);
         preOrder(root -> right,ans,digit);
 
     }
     int sumRootToLeaf(TreeNode* root) {
         int ans = 0;
-        // postOrder(root,ans,0);
         preOrder(root,ans,0);
         return ans;
     }
