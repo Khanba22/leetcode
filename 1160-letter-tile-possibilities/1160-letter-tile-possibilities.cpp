15515ffec53685ab6f554941177297827a13e1bb
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-17 09:59:09 +0530

    Time: 6 ms (54.86%), Space: 8 MB (84.16%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -8,14 +8,14 @@ class Solution {
 public:
     int numTilePossibilities(string tiles) {
         vector<int> freq(26, 0);
-        for (char c : tiles) freq[c - 'A']++;
+        for (char &c : tiles) freq[c - 'A']++;
         return backtrack(freq);
     }
 
     int backtrack(vector<int>& freq) {
         int count = 0;
         for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
+            if (freq[i]) {
                 count++;
                 freq[i]--;
                 count += backtrack(freq);
