8c7dcc14c444c3cf77ccb50b38e15fbb7f3b7482
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-17 10:01:19 +0530

    Time: 3 ms (78.62%), Space: 8 MB (84.16%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 
 using namespace std;
 
@@ -8,20 +7,22 @@ class Solution {
 public:
     int numTilePossibilities(string tiles) {
         vector<int> freq(26, 0);
-        for (char &c : tiles) freq[c - 'A']++;
-        return backtrack(freq);
+        for (char c : tiles) freq[c - 'A']++;
+
+        return backtrack(freq, tiles.size());
     }
 
-    int backtrack(vector<int>& freq) {
+    int backtrack(vector<int>& freq, int remaining) {
         int count = 0;
         for (int i = 0; i < 26; i++) {
-            if (freq[i]) {
-                count++;
-                freq[i]--;
-                count += backtrack(freq);
-                freq[i]++;
-            }
+            if (freq[i] == 0) continue;
+
+            count++;
+            freq[i]--;
+            count += backtrack(freq, remaining - 1);
+            freq[i]++;
         }
         return count;
     }
 };
+
