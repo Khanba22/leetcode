8efc6d65add5cbfedd55a01abfbbdd5f6ffe2554
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-18 11:47:49 +0530

    Time: 18 ms (22.92%), Space: 7.9 MB (73.43%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -8,21 +8,20 @@ public:
             return;
         }
 
-        for (char i = '1'; i <= '9'; i++) {
-            if (visited[i - '0']) continue;
-
-            if (!curr.empty()) {
+        for (int i = 1; i <= 9; i++) {
+            if (visited[i]) continue;
+            char ci = i + '0';
+            if (index) {
                 char prev = curr.back();
-                if ((pattern[index - 1] == 'I' && i <= prev) || 
-                    (pattern[index - 1] == 'D' && i >= prev)) {
+                if ((pattern[index - 1] == 'I' && ci <= prev) || 
+                    (pattern[index - 1] == 'D' && ci >= prev)) {
                     continue;
                 }
             }
-
-            curr.push_back(i);
-            visited[i - '0'] = true;
+            curr.push_back(ci);
+            visited[i] = true;
             helper(pattern, index + 1, visited, curr, res);
-            visited[i - '0'] = false;
+            visited[i] = false;
             curr.pop_back();
         }
     }
