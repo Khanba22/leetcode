6985be5edd7a6008df7a13479e70b6f380cd7442
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-18 11:48:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (42.39%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    void helper(string &pattern, int index, vector<bool>& visited, string &curr, string &res) {
-        if (index == pattern.size() + 1) {
-            if (res.empty() || curr < res) {
-                res = curr;
-            }
-            return;
-        }
+    string smallestNumber(string pattern) {
+        int n = pattern.length();
+        string result = "";
+        int stack[n + 1];
+        int index = 0;
 
-        for (int i = 1; i <= 9; i++) {
-            if (visited[i]) continue;
-            char ci = i + '0';
-            if (index) {
-                char prev = curr.back();
-                if ((pattern[index - 1] == 'I' && ci <= prev) || 
-                    (pattern[index - 1] == 'D' && ci >= prev)) {
-                    continue;
+        for (int i = 0; i <= n; i++) {
+            stack[index++] = i + 1;
+
+            if (i == n || pattern[i] == 'I') {
+                while (index > 0) {
+                    result += to_string(stack[--index]);
                 }
             }
-            curr.push_back(ci);
-            visited[i] = true;
-            helper(pattern, index + 1, visited, curr, res);
-            visited[i] = false;
-            curr.pop_back();
         }
-    }
 
-    string smallestNumber(string pattern) {
-        vector<bool> visited(10, false);
-        string res, curr;
-        helper(pattern, 0, visited, curr, res);
-        return res;
+        return result;
     }
 };
