36325f4d707cfeb9bac8376a4ae86afbba967828
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-18 12:00:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.6%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,16 +1,20 @@
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        short n = pattern.length();
-        string result = "";
-        short stack[n + 1];
-        short index = 0;
-        for (short i = 0; i <= n; i++) {
-            stack[index++] = i + 1;
+        unsigned char n = pattern.size();
+        string result;
+        char stack[11];
+        uint8_t index = 0; 
 
+        for (uint8_t i = 0; i <= n; i++) {
+            stack[index++] = '1' + i;
+            
             if (i == n || pattern[i] == 'I') {
                 while (index > 0) {
-                    result.push_back(stack[--index] + '0');
+                    result.push_back(stack[--index]);
                 }
             }
         }
