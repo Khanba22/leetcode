619daf465a120e601ebaeca9ee0e9e944581955d
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-19 15:19:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.26%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool helper(int n, int k, string &curr, int index, string &res, int &count) {
+        if (index == n) {
+            count++;
+            if (count == k) {
+                res = curr;
+                return true; 
+            }
+            return false;
+        }
+
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (!curr.empty() && curr.back() == ch) continue; 
+
+            curr.push_back(ch);
+            if (helper(n, k, curr, index + 1, res, count)) return true; 
+            curr.pop_back(); 
+        }
+        return false;
+    }
+
+    string getHappyString(int n, int k) {
+        int count = 0;
+        string res = "";
+        string curr = "";
+        helper(n, k, curr, 0, res, count);
+        return res;
+    }
+};
