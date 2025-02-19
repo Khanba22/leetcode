ff2103b60990b0d3a32c0e002f6316baa2d3ed62
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-19 15:27:05 +0530

    Time: 3 ms (74.09%), Space: 8 MB (87.05%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool helper(int &n, int &k, string &curr, int &index, string &res, int &count) {
+    bool helper(int &n, int &k, string &curr, int index, string &res) {
         if (index == n) {
-            count++;
-            if (count == k) {
+            k--;
+            if (k == 0) {
                 res = curr;
                 return true; 
             }
             return false;
         }
-        index++;
+
         for (char ch = 'a'; ch <= 'c'; ch++) {
             if (!curr.empty() && curr.back() == ch) continue; 
             curr.push_back(ch);
-            if (helper(n, k, curr, index, res, count)) return true; 
+            if (helper(n, k, curr, index + 1, res)) return true; 
             curr.pop_back(); 
         }
-        index--;
         return false;
     }
 
     string getHappyString(int n, int k) {
-        int count = 0;
         string res = "";
         string curr = "";
-        int index = 0;
-        helper(n, k, curr, index, res, count);
+        helper(n, k, curr, 0, res);
         return res;
     }
 };
