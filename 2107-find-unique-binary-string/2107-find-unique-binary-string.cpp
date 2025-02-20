575faafa0b94dd7742c091ad801d679828efb414
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-20 20:04:26 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.88%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,1 +1,10 @@
-class Solution{public:string findDifferentBinaryString(vector<string>&nums){string ans;for(int i=0;i<nums.size();i++){ans.push_back(nums[i][i]=='1'?'0':'1');}return ans;}};
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans(nums.size(),'0');
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i]=nums[i][i] == '1' ? '0' : '1';
+        }
+        return ans;
+    }
+};
