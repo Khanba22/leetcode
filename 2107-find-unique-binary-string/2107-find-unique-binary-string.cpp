a46795d4782be28adab717540ccd829610c29fe8
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-20 18:39:21 +0530

    Time: 0 ms (100%), Space: 13 MB (38.12%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<int> seen;
+        int n = nums[0].size();
+        string startstr(n, '0');
+        string endstr(n, '1');
+        int start = 0;
+        int end = stoi(endstr, 0, 2);
+        for (string s : nums) {
+            seen.insert(stoi(s, 0, 2));
+        }
+        while (start <= end) {
+            if (!seen.count(start)) {
+                return toBinaryString(start, n);
+            }
+            start++;
+        }
+        return toBinaryString(start, n);
+    }
+
+    string toBinaryString(int num, int length) {
+        string result = "";
+        while (num > 0) {
+            result = (char)('0' + (num % 2)) + result;
+            num /= 2;
+        }
+        while (result.size() < length) {
+            result = '0' + result;
+        }
+        return result;
+    }
+};
+
