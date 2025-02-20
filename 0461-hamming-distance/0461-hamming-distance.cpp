25ca8d60cd0520357da36e642c09ac0d72e561b5
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-20 20:16:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.77%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int dist = 0;
-        while(x && y){
-            dist+=!(x%2 == y%2);
-            x/=2;
-            y/=2;
-        }
-        while(x){
-            dist+=x%2;
-            x/=2;
-        }
-        while(y){
-            dist+=y%2;
-            y/=2;
+        while (x || y) { 
+            dist += (x & 1) != (y & 1); 
+            x >>= 1;
+            y >>= 1;
         }
         return dist;
     }
