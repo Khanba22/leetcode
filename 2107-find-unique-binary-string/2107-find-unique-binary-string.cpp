380fd9a728d56ddbd473d7a27091208078884912
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-20 19:58:17 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -5,10 +5,10 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++){
             if(nums[i][i]=='1'){
-                ans += '0';
+                ans.push_back('0');
             }
             else{
-                ans += '1';
+                ans.push_back('1');
             }
 
         }
