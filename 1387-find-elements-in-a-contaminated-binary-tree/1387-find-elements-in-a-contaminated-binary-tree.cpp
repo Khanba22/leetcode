d6612c4a70266d6a7ea13ec141d83f1eae42d81b
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-21 19:17:48 +0530

    Time: 7 ms (82.43%), Space: 27.4 MB (77.76%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -1,37 +1,31 @@
 class FindElements {
 private:
-    unordered_map<int,int> seen;
+    unordered_map<int, int> seen;
+
 public:
     FindElements(TreeNode* root) {
         if (root) {
             root->val = 0;
-            bfs(root);
-        }
-    }
+            queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty()) {
+                TreeNode* node = q.front();
+                q.pop();
 
-    bool find(int target) {
-        return seen.count(target);
-    }
+                int val = node->val;
+                seen[val]++;
 
-    void bfs(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            int val = node->val;
-            seen[val]++;
-
-            if (node->left) {
-                node->left->val = 2 * val + 1;
-                q.push(node->left);
-            }
-            if (node->right) {
-                node->right->val = 2 * val + 2;
-                q.push(node->right);
+                if (node->left) {
+                    node->left->val = 2 * val + 1;
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    node->right->val = 2 * val + 2;
+                    q.push(node->right);
+                }
             }
         }
     }
+
+    bool find(int target) { return seen.count(target); }
 };
