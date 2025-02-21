a5877e7bc640accc663a2417eb981c1a0309bd69
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-21 22:51:46 +0530

    Time: 430 ms (5.71%), Space: 7.9 MB (89.79%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    string getPermutation(int n, int k) {
-        vector<int> numbers;
-        int fact = 1;
-        for (int i = 1; i <= n; i++) {
-            numbers.push_back(i);
-            if (i < n) fact *= i;
+    bool helper(int n, int k, vector<bool>& visited, string& res, int& count, string& curr) {
+        if (curr.size() == n) {
+            count++;  
+            if (count == k) { 
+                res = curr;
+                return true;
+            }
+            return false;
         }
-        
-        k--;
-        string res = "";
 
-        for (int i = 0; i < n; i++) {
-            int index = k / fact;
-            res += to_string(numbers[index]);  
-            numbers.erase(numbers.begin() + index);
-            
-            if (i < n - 1) {
-                k %= fact;
-                fact /= (n - 1 - i);
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                curr.push_back(i + '0');
+
+                if (helper(n, k, visited, res, count, curr)) return true;
+
+                visited[i] = false;
+                curr.pop_back();
             }
         }
-        
+        return false;
+    }
+
+    string getPermutation(int n, int k) {
+        vector<bool> visited(n + 1, false);
+        string res = "";
+        int count = 0;
+        string curr = "";
+
+        helper(n, k, visited, res, count, curr);
         return res;
     }
 };
