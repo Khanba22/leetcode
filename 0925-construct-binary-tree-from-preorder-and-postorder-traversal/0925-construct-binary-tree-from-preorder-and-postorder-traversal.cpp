02a593d0377ed894350db7a6a8f8fa71134247f4
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-23 10:18:44 +0530

    Time: 0 ms (100%), Space: 28.2 MB (56.32%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> x2pre, x2post;
+
+    TreeNode* helper(vector<int>& preorder, vector<int>& postorder, int preS, int postS, int postE) {
+        if (postS>postE) return NULL; 
+        TreeNode* root=new TreeNode(preorder[preS]);
+        if (postS==postE) return root;
+        
+        int lRoot=preorder[preS+1];
+        int lrPostIdx=x2post[lRoot];
+
+        root->left=helper(preorder, postorder, preS+1, postS, lrPostIdx);
+        root->right=helper(preorder, postorder, x2pre[postorder[postE-1]], lrPostIdx+1, postE-1);
+
+        return root;
+    }
+
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        const int n = preorder.size();
+        x2pre.assign(n+1, -1);
+        x2post.assign(n+1, -1);
+        
+        for (int i=0; i<n; i++) {
+            x2pre[preorder[i]]=i;
+            x2post[postorder[i]]=i;
+        }
+
+        return helper(preorder, postorder, 0, 0, n-1);
+    }
+};
+
+
