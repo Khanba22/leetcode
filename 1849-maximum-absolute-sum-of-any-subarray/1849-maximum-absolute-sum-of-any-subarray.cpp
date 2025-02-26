158dc71c4f5998f43887d915790c2393b5b0e5a2
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-02-26 10:11:18 +0530

    Time: 0 ms (100%), Space: 44.8 MB (98.76%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,3 +1,12 @@
+static int speedup = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    return 0;
+}();
+#pragma GCC optimize("O3", "unroll-loops")
+
+
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
