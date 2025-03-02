867b50a7c3283145caf2f9c226a864ff21af24d8
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-02 10:57:46 +0530

    Time: 0 ms (100%), Space: 14.2 MB (99.87%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -3,34 +3,30 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& n1,
                                     vector<vector<int>>& n2) {
         int i = 0, j = 0;
+        int m = n1.size(), n = n2.size();
         vector<vector<int>> res;
-        int n = n2.size();
-        int m = n1.size();
+        res.reserve(m + n);  // Preallocate memory
+
         while (i < m && j < n) {
-            int k1 = n1[i][0];
-            int k2 = n2[j][0];
-            int v1 = n1[i][1];
-            int v2 = n2[j][1];
-            if (k1 == n2[j][0]) {
+            int k1 = n1[i][0], v1 = n1[i][1]; 
+            int k2 = n2[j][0], v2 = n2[j][1];
+
+            if (k1 == k2) {
                 res.push_back({k1, v1 + v2});
                 i++;
                 j++;
             } else if (k1 < k2) {
-                res.push_back({k1, v1});
+                res.push_back(move(n1[i])); 
                 i++;
             } else {
-                res.push_back({k2, v2});
+                res.push_back(move(n2[j]));
                 j++;
             }
         }
-        while (i < m) {
-            res.push_back({n1[i][0], n1[i][1]});
-            i++;
-        }
-        while (j < n) {
-            res.push_back({n2[j][0], n2[j][1]});
-            j++;
-        }
+        
+        while (i < m) res.push_back(move(n1[i++]));
+        while (j < n) res.push_back(move(n2[j++]));
+
         return res;
     }
 };
