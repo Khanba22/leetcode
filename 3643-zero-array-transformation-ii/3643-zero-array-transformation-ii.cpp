b35d60d3c78b662c1314ffe717a68d2933f8066d
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-13 15:43:36 +0530

    Time: 0 ms (100%), Space: 323 MB (85.62%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,39 +1,24 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    bool canMakeZero(vector<int>& nums, vector<vector<int>>& queries, int k) {
-        int n = nums.size();
-        vector<int> diff(n + 1, 0);
-
-        for (int i = 0; i < k; i++) {
-            diff[queries[i][0]] += queries[i][2];
-            diff[queries[i][1] + 1] -= queries[i][2];
-        }
-
-        int curr = 0;
-        for (int i = 0; i < n; i++) {
-            curr += diff[i];
-            if (curr < nums[i]) return false;
-        }
-        return true;
-    }
-
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), m = queries.size();
-        
-        if (all_of(nums.begin(), nums.end(), [](int x) { return x == 0; })) return 0;
-        if (!canMakeZero(nums, queries, m)) return -1;
-
-        int left = 1, right = m;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canMakeZero(nums, queries, mid)) right = mid;
-            else left = mid + 1;
+        int n = nums.size(), sum = 0, k = 0;
+        vector<int> cnt(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            while (sum + cnt[i] < nums[i]) {
+                if (k == queries.size()) return -1;
+                int l = queries[k][0];
+                int r = queries[k][1];
+                int val = queries[k][2];
+                k++;
+                
+                if (r < i) continue;
+                cnt[max(l, i)] += val;
+                cnt[r + 1] -= val;
+            }
+            sum += cnt[i];
         }
-        return left;
+        return k;
     }
 };
+
+
