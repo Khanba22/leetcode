8f61ab20edbaafb3d3047621b6c86574fd9dd1a5
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-14 21:04:17 +0530

    Time: 19 ms (84.93%), Space: 88.2 MB (41.19%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,29 +1,21 @@
 class Solution {
-public:
-    bool isSufficient(vector<int>& candies, long long k, int amount) {
-        if (amount == 0) return false;
-        long long count = 0;
-        for (int &c : candies) {
-            count += c / amount;
-            if (count >= k) return true;
+    bool isPossible(vector<int>& can, long long mid, long long k) {
+        long long total = 0;
+        for(auto x: can) {
+            total += x / mid;
         }
-        return false;
+        return total >= k;
     }
-
-    int maximumCandies(vector<int>& candies, long long k) {
-        int low = 1, high = 0, res = 0;
-        for(int &i:candies){
-            high = max(high,i);    
+public:
+    int maximumCandies(vector<int>& can, long long k) {
+        long long n = can.size(), end = 0, start = 1;
+        for(auto x: can) end += x;
+        end /= k;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(isPossible(can, mid, k)) start = mid + 1;
+            else end = mid - 1;
         }
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isSufficient(candies, k, mid)) {
-                res = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return res;
+        return end;
     }
 };
