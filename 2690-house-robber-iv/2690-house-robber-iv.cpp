c079196ea2e3c9cafe2579a8c96eea9820a51107
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-15 16:03:42 +0530

    Time: 28 ms (40.75%), Space: 60.9 MB (40.42%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canSteal(const vector<int>& nums, int k, int cap) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] <= cap) {
+                count++;
+                i += 2; 
+            } else {
+                i++;
+            }
+        }
+        return count >= k;
+    }
+    
+    int minCapability(vector<int>& nums, int k) {
+        int left = INT_MAX;
+        int right = INT_MIN;
+        for(int i = 0;i < nums.size();i++){
+            left = min(left,nums[i]);
+            right = max(right,nums[i]);
+        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canSteal(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
