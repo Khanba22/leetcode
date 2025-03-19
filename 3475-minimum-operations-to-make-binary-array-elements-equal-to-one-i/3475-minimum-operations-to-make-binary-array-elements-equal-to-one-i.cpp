36f231509f74cf8a966e253d784e4872300948ea
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-19 15:10:55 +0530

    Time: 6 ms (64.84%), Space: 140 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
 
         for(int i = 0; i <= n - 3; i++) {
-            if(!nums[i]) {
+            if(nums[i] == 0) {
                 ops++;
                 nums[i] = 1 - nums[i];
                 nums[i + 1] = 1 - nums[i + 1];
