2df1f0db1623e8da1e55cefd3c6bbf34843b2871
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-26 14:52:30 +0530

    Time: 48 ms (37.99%), Space: 90.1 MB (48.02%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,25 +4,20 @@ public:
         vector<int> values;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[i].size(); j++) {
-                values.emplace_back(grid[i][j]);
+                values.push_back(grid[i][j]);
             }
         }
 
         sort(values.begin(), values.end());
+        int median = values[values.size() / 2];
+        int operations = 0;
 
         for (int val : values) {
             if (abs(val - values[0]) % x != 0) {
                 return -1;
             }
-        }
-
-        int median = values[values.size() / 2];
-        int operations = 0;
-
-        for (int val : values) {
             operations += abs(val - median) / x;
         }
-
         return operations;
     }
 };
