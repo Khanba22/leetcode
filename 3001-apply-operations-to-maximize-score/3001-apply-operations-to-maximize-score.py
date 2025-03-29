7a47b749310667f7dc2e2a9efe90ed5b2cf1230d
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-29 17:56:31 +0530

    Time: 393 ms (100%), Space: 46 MB (31.58%) - LeetHub

--- 3001-apply-operations-to-maximize-score/3001-apply-operations-to-maximize-score.py
+++ 3001-apply-operations-to-maximize-score/3001-apply-operations-to-maximize-score.py
@@ -1,49 +1,31 @@
-def primeFactors(n):
-    i = 2
-    ans = set()
-    while i * i <= n:
-        while n % i == 0:
-            ans.add(i)
-            n //= i
-        i += 1
-    if n > 1:
-        ans.add(n)
-    return len(ans)
-
+MOD = 10 ** 9 + 7
+MX = 10 ** 5 + 1
+omega = [0] * MX
+for i in range(2, MX): 
+    if omega[i] == 0: 
+        for j in range(i, MX, i): 
+            omega[j] += 1
 
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
-        mod = 10**9 + 7
-        arr = [(i, primeFactors(x), x) for i, x in enumerate(nums)]
         n = len(nums)
-
         left = [-1] * n
         right = [n] * n
-        stk = []
-        for i, f, x in arr:
-            while stk and stk[-1][0] < f:
-                stk.pop()
-            if stk:
-                left[i] = stk[-1][1]
-            stk.append((f, i))
+        st = []
+        for i, v in enumerate(nums): 
+            while st and omega[nums[st[-1]]] < omega[v]: 
+                right[st.pop()] = i
+            if st: 
+                left[i] = st[-1]
+            st.append(i)
 
-        stk = []
-        for i, f, x in arr[::-1]:
-            while stk and stk[-1][0] <= f:
-                stk.pop()
-            if stk:
-                right[i] = stk[-1][1]
-            stk.append((f, i))
-
-        arr.sort(key=lambda x: -x[2])
         ans = 1
-        for i, f, x in arr:
-            l, r = left[i], right[i]
-            cnt = (i - l) * (r - i)
-            if cnt <= k:
-                ans = ans * pow(x, cnt, mod) % mod
-                k -= cnt
-            else:
-                ans = ans * pow(x, k, mod) % mod
+        for i, v, l, r in sorted(zip(range(n), nums, left, right), key=lambda x: -x[1]): 
+            tot = (i - l) * (r - i)
+            if tot >= k: 
+                ans = ans * pow(v, k, MOD) % MOD
                 break
+            ans = ans * pow(v, tot, MOD) % MOD
+            k -= tot
+        
         return ans
