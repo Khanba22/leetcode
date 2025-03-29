ccb285fefa48fef6c9ac56b9eec4d10c97495fe1
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-03-29 18:12:06 +0530

    Time: 0 ms (100%), Space: 17 MB (95.61%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,23 +1,34 @@
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> maxHeap; 
-        
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[i].size(); j++) {
-                maxHeap.push(matrix[i][j]);
-                
-                if (maxHeap.size() > k) {
-                    maxHeap.pop();
-                }
+    int check(vector<vector<int>>& matrix,int mid) {
+        int count = 0;
+        int n = matrix.size();
+        int row = 0;
+        int col = n - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] <= mid){
+                count += (col + 1);
+                row++;
+            }else{
+                col--;
             }
         }
-        
-        return maxHeap.top();
+        return count; 
+    }
+
+    int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (check(matrix,mid) < k){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low; 
     }
 };
