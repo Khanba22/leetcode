0980eb672e3132f6fafb7d568eb1d17e2ecfe6ad
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-04-08 10:46:12 +0530

    Time: 0 ms (100%), Space: 27.1 MB (96.93%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        unordered_map<int, int> T;  
-        unordered_map<int, int> D;  
+        bool e[101] = {0};
+        int size = nums.size();
 
-        for (int a : nums) {
-            T[a]++;
-            if (T[a] > 1) {
-                D[a]++;
-            }
+        for (int i = size - 1; i >= 0; --i) {
+            if (e[nums[i]])
+                return ceil(double(i + 1) / 3);
+            e[nums[i]] = true;
         }
-
-        int ans = 0;
-        int i = 0;
-        int n = nums.size();
-
-        while (!D.empty()) {
-            if ((n - i) < 3) {
-                ans++;
-                break;
-            } else {
-                int x = i++;
-                int y = i++;
-                int z = i++;
-
-                if (D.count(nums[x])) {
-                    if (--D[nums[x]] == 0) D.erase(nums[x]);
-                }
-                if (D.count(nums[y])) {
-                    if (--D[nums[y]] == 0) D.erase(nums[y]);
-                }
-                if (D.count(nums[z])) {
-                    if (--D[nums[z]] == 0) D.erase(nums[z]);
-                }
-
-                ans++;
-            }
-        }
-
-        return ans;
+        
+        return 0;
     }
 };
