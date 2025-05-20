7cb63a9f7e9510a5ee0f53c21f9fbccccb1218f6
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-05-20 10:58:41 +0530

    Time: 0 ms (100%), Space: 293.1 MB (42.23%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>prefix(nums.size()+1,0);
+        int n = nums.size();
+        vector<int>prefix(n+1,0);
         for(int i = 0;i < queries.size();i++){
             prefix[queries[i][0]]++;
             prefix[queries[i][1] + 1]--;
         }
         int curr = 0;
-        for(int i = 0;i < nums.size();i++){
+        for(int i = 0;i < n;i++){
             curr+=prefix[i];
             if(nums[i] - curr > 0) return false;
         }
