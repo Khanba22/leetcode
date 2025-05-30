1a871a953f9a3c0b0f622bca0b66886cf780d77b
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2025-05-30 12:18:38 +0530

    Time: 66 ms (82.31%), Space: 14.1 MB (91.21%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int res = 0;
+        int n = s.size();
+        vector<int> minIdx(26, -1), maxIdx(26, -1);
+        
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            if (minIdx[idx] == -1) minIdx[idx] = i;
+            maxIdx[idx] = i;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (minIdx[i] != -1 && minIdx[i] < maxIdx[i]) {
+                vector<bool> seen(26, false);
+                for (int j = minIdx[i] + 1; j < maxIdx[i]; j++) {
+                    seen[s[j] - 'a'] = true;
+                }
+                res += count(seen.begin(), seen.end(), true);
+            }
+        }
+
+        return res;
+    }
+};
